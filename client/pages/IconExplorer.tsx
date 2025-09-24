2bdd1a22c42212c50d07b4c0bb5384982369f20e
Cheathek <cheathekm@gmail.com>
2025-09-24 20:26:47 +0700

    yokyok

--- client/pages/IconExplorer.tsx
+++ client/pages/IconExplorer.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useMemo, useState, useRef } from "react";
 import { useSearchParams } from "react-router-dom";
-import { IconSearch, IconChevronDown, IconLayoutGrid, IconListDetails, IconRestore, IconSquareRoundedCheckFilled, IconSlash } from "@tabler/icons-react";
+import { IconSearch, IconChevronDown, IconLayoutGrid, IconListDetails, IconSquareRoundedCheckFilled, IconSlash } from "@tabler/icons-react";
 import { Input } from "@/components/ui/input";
 import { Tabs, TabsContent } from "@/components/ui/tabs";
 import Box from "@mui/material/Box";
