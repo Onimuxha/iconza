9384964e6db94762165c92a7adfeeec067945030
Onimuxha <cheathekm@gmail.com>
2025-10-04 14:26:18 +0700

    fix

--- server/index.ts
+++ server/index.ts
@@ -2,7 +2,7 @@ import "dotenv/config";
 import express from "express";
 import cors from "cors";
 import { handleDemo } from "./routes/demo";
-import { handleSitemap } from "../scripts/generate-sitemap";
+// import { handleSitemap } from "../scripts/generate-sitemap";
 
 export function createServer() {
   const app = express();
@@ -19,7 +19,7 @@ export function createServer() {
   });
 
   app.get("/api/demo", handleDemo);
-  app.get("/sitemap.xml", handleSitemap)
+  // app.get("/sitemap.xml", handleSitemap)
 
   return app;
 }
